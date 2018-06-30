aa29f38367b94bd0e43c8a80258bc429c0c1352c
Alex Dalton <alex.k.dalton@gmail.com>
2018-06-30 16:46:33 +0100

    using 'dev_appserver.py ./' one can launch a local server

--- cdn.py
+++ cdn.py
@@ -1,1 +1,21 @@
+import os
+import urllib
 
+from google.appengine.api import users
+from google.appengine.ext import ndb
+
+import jinja2
+import webapp2
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+        loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+        extensions=['jinja2.ext.autoescape'],
+        autoescape=True)
+
+class DemoForm(webapp2.RequestHandler):
+    def get(self):
+        self.response.write("index.html")
+
+app = webapp2.WSGIApplication([
+    ('/',DemoForm)
+],debug=True)
