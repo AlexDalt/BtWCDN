7e709b53cb33f84249be07e95ba1e960a03d2c9a
Alex Dalton <alex.k.dalton@gmail.com>
2018-10-12 21:43:16 +0100

    VOTING WORKS

--- cdn.py
+++ cdn.py
@@ -3,6 +3,8 @@ import urllib
 
 from google.appengine.api import users
 from google.appengine.ext import ndb
+import time
+
 
 import jinja2
 import webapp2
@@ -18,10 +20,9 @@ class Application(ndb.Model):
     facebook = ndb.StringProperty(indexed=False)
     soundcloud = ndb.StringProperty(indexed=False)
     contact = ndb.StringProperty(indexed=False)
+    votes_for = ndb.IntegerProperty(indexed=False,repeated=True)
+    votes_against = ndb.IntegerProperty(indexed=False,repeated=True)
 
-    reviewer = ndb.IntegerProperty(indexed=True)
-    reviewed = ndb.BooleanProperty(indexed=True)
-    shortlisted = ndb.BooleanProperty(indexed=True)
 
 class DemoForm(webapp2.RequestHandler):
     def get(self):
@@ -37,24 +38,48 @@ class Submit(webapp2.RequestHandler):
         application.facebook = self.request.get('facebook')
         application.soundcloud = self.request.get('soundcloud')
         application.contact = self.request.get('contact')
+        application.votes_for = []
+        application.votes_against = []
 
-        application.reviewer = 0
-        application.reviewed = False
-        application.shortlisted = False
         application.put()
 
         self.redirect('/')
 
 class Review(webapp2.RequestHandler):
     def get(self):
-        applications_query = Application.query(Application.reviewer == 0)
+        applications_query = Application.query()
         applications = applications_query.fetch()
         template_values = {'applications': applications}
         template = JINJA_ENVIRONMENT.get_template('review.html')
         self.response.write(template.render(template_values))
 
+class Vote(webapp2.RequestHandler):
+    def post(self):
+        # get user ID, store that in a list on data object
+        user_id = 1;
+        band_key = ndb.Key(urlsafe=self.request.get('band_key'))
+
+        application = band_key.get()
+
+        if( self.request.get('vote') == "yes" ):
+            if user_id in application.votes_against:
+                application.votes_against.remove(user_id)
+            if user_id not in application.votes_for:
+                application.votes_for.append(user_id)
+        elif( self.request.get('vote') == "no" ):
+            if user_id in application.votes_for:
+                application.votes_for.remove(user_id)
+            if user_id not in application.votes_against:
+                application.votes_against.append(user_id)
+
+        application.put()
+        time.sleep(0.2)
+        self.redirect('/review')
+
+
 app = webapp2.WSGIApplication([
     ('/',DemoForm),
     ('/submit', Submit),
     ('/review', Review),
+    ('/vote', Vote),
 ],debug=True)
